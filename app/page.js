876f73659f3e187bf2b63368b2ca85ee2af08297
bayuaji000 <bayuaji.2999@gmail.com>
2025-05-07 00:28:57 +0700

    add link to Resume Button

--- app/page.js
+++ app/page.js
@@ -29,7 +29,7 @@ export default function Home() {
               <li>
                 <a
                   className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 border-none rounded-md ml-4 sm:ml-8"
-                  href="#"
+                  href="https://drive.google.com/file/d/1d4uh9-z9i8vy9XfFcVaZif7mkumORbHb/view?usp=sharing"
                 >
                   Resume
                 </a>
