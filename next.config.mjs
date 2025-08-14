8f94f9519b706c18b2faf9a6c18c650f90083705
bayuaji000 <bayuaji.2999@gmail.com>
2025-08-14 15:12:18 +0700

    add new project

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
+<<<<<<< HEAD
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
 export default nextConfig;
+=======
+// next.config.mjs
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'synthova.de',
+                pathname: '/wp-content/uploads/**',
+            },
+        ],
+    },
+}
+
+export default nextConfig
+>>>>>>> 76c4d78 (add new project)
