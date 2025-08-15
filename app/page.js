9fb5c4fea28fa0eff1b44b8d6b66b961c4be9051
M. Bayu Aji <123294774+bayuaji000@users.noreply.github.com>
2025-08-15 08:54:03 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -76,7 +76,7 @@ export default function Home() {
               <li>
                 <a
                   className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 border-none rounded-md ml-4 sm:ml-8"
-                  href="https://drive.google.com/file/d/1Lo28jN4R4Ej4_B0b0Zleg4eAmzpz0vHN/view?usp=drive_link"
+                  href="https://drive.google.com/file/d/1N_XSpwKAryhxu8MOT6irEROLVaKEKOP5/view?usp=drivesdk"
                 >
                   Resume
                 </a>
